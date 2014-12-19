8201c4ee1c4dd546a278243fad7300e69e550884
Spencer Elliott <me@elliottsj.com>
2014-12-18 21:51:17 -0500

    Add echo handler for testing

--- handlers/gif_limiter.rb
+++ handlers/gif_limiter.rb
@@ -1,6 +1,11 @@
 module Lita
   module Handlers
     class GifLimiter < Handler
+      route(/^echo\s+(.+)/, command: true, help: {
+        :'echo TEXT' => 'Replies back with TEXT.'
+      }) do |response|
+        response.reply response.message
+      end
     end
 
     Lita.register_handler(GifLimiter)
